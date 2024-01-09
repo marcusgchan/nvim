cc81fb693bf1a528931990846df3a5d4b6fea3df
Marcus Chan <marcuscchan@gmail.com>
2024-01-09 01:15:48 +0000

    Add comment, undotree, and copilot

---
+++ lua/marcus/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-i>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-u>",
+					prev = "<C-o>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = false,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 18.x
+			server_opts_overrides = {},
+		})
+	end,
+}
--- lua/marcus/plugins/lsp/lspconfig.lua
+++ lua/marcus/plugins/lsp/lspconfig.lua
@@ -1,82 +1,82 @@
 return {
-  "neovim/nvim-lspconfig",
- -- event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local keymap = vim.keymap
+	"neovim/nvim-lspconfig",
+	-- event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local keymap = vim.keymap
 
-    local opts = { noremap = true }
-    local on_attach = function(client, bufnr)
-      opts.bufnr = bufnr
-      print("on_attach")
-      keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      keymap.set("n", "K", vim.lsp.buf.hover, opts)
-      keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-      keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-      keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-      keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-      keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-      keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-      keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-      keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-    end
+		local opts = { noremap = true }
+		local on_attach = function(client, bufnr)
+			opts.bufnr = bufnr
 
-    -- used to enable autocompletion (assign to every lsp server config)
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+			keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+			keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+			keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+			keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+			keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+			keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+			keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+			keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+		end
 
-    lspconfig["html"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    lspconfig["tsserver"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["html"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["cssls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["tsserver"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["tailwindcss"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["cssls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["prismals"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["tailwindcss"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
+		lspconfig["prismals"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-    lspconfig["lua_ls"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
-  end,
+		lspconfig["pyright"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		lspconfig["lua_ls"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = { -- custom settings for lua
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						-- make language server aware of runtime files
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
+						},
+					},
+				},
+			},
+		})
+	end,
 }
