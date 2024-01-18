942818e16c95fd0ba88cc22d3c1d47c715903f12
Marcus Chan <marcuscchan@gmail.com>
2024-01-18 03:52:09 +0000

    Fix svelte not workign

--- lua/marcus/plugins/lsp/lspconfig.lua
+++ lua/marcus/plugins/lsp/lspconfig.lua
@@ -49,9 +49,7 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePost", {
 					pattern = { "*.js", "*.ts" },
 					callback = function(ctx)
-						if client.name == "svelte" then
-							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-						end
+						client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
 					end,
 				})
 			end,
