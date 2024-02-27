500b0cf8a2773f498744a5806dac3b1d851250db
Marcus Chan <marcuscchan@gmail.com>
2024-02-27 15:04:21 -0800

    Add snips

--- lua/marcus/plugins/linter.lua
+++ lua/marcus/plugins/linter.lua
@@ -1,6 +1,15 @@
 return {
 	"mfussenegger/nvim-lint",
 	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		linters = {
+			eslint_d = {
+				args = {
+					"--no-warn-ignored",
+				},
+			},
+		},
+	},
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
