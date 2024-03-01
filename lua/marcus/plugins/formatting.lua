1bc51fc6474d300e43473d20ac8fcdaa3d32d5e1
Marcus Chan <marcuscchan@gmail.com>
2024-03-01 09:35:07 -0800

    Add prettier to run ts

--- lua/marcus/plugins/formatting.lua
+++ lua/marcus/plugins/formatting.lua
@@ -7,6 +7,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettier" },
+				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				svelte = { "prettier" },
 				css = { "prettier" },
