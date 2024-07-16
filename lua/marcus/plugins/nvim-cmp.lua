0d15e04c3b1e71490149e71fbc9fc7c522f7da40
Marcus Chan <marcuscchan@gmail.com>
2024-07-16 10:46:58 -0700

    explicitly set cmp bindings

--- lua/marcus/plugins/nvim-cmp.lua
+++ lua/marcus/plugins/nvim-cmp.lua
@@ -22,7 +22,10 @@ return {
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<C-e>"] = cmp.mapping.abort(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
