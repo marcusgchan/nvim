83fbce422dd6e18f752c147fab272887d544e1aa
Marcus Chan <marcuscchan@gmail.com>
2024-10-01 11:26:35 -0700

    stuffs

--- lua/marcus/core/init.lua
+++ lua/marcus/core/init.lua
@@ -5,5 +5,6 @@ local venv = os.getenv("VIRTUAL_ENV")
 if venv then
 	vim.g.python3_host_prog = venv .. "/bin/python"
 else
-	vim.g.python3_host_prog = "/usr/bin/python3"
+	vim.g.python3_host_prog = "~/.pyenv/shims/python"
 end
+-- vim.g.python3_host_prog = venv .. "/bin/python"
